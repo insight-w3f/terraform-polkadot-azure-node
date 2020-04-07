bb4bf846e58b216664d778989ab69f384810159b
Richard Mah <dick.mah@gmail.com>
2020-04-07 10:32:59 -0400

    update default tests and CCI config

--- examples/defaults/main.tf
+++ examples/defaults/main.tf
@@ -1,4 +1,18 @@
+provider "azurerm" {
+  version = "=2.0.0"
+  features {}
+}
+
+resource "azurerm_resource_group" "this" {
+  location = "eastus"
+  name     = "asg-default-testing"
+}
+
+variable "public_key_path" {}
+variable "private_key_path" {}
 
 module "defaults" {
-  source = "../.."
+  source           = "../.."
+  public_key_path  = var.public_key_path
+  private_key_path = var.private_key_path
 }
