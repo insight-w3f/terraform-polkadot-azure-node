1a2e15abda66f72d894bc3ad9b0ff6d5e1f5e1ec
Richard Mah <dick.mah@gmail.com>
2020-05-12 19:30:25 -0400

    update outputs for correct type

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
 output "public_ip" {
-  value = azurerm_linux_virtual_machine.this.*.public_ip_addresses
+  value = azurerm_linux_virtual_machine.this.0.public_ip_address
 }
 
 output "private_ip" {
-  value = azurerm_linux_virtual_machine.this.*.private_ip_addresses
+  value = azurerm_linux_virtual_machine.this.0.private_ip_address
 }
 
 output "user_data" {
-  value = azurerm_linux_virtual_machine.this.*.custom_data
+  value = azurerm_linux_virtual_machine.this.0.custom_data
 }
